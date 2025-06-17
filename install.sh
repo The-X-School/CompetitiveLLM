f1630e18060eb927e1b71fb3e92e88fa328527a9
Charlie Huang <charliehuang09@gmail.com>
2025-06-17 09:27:23 -0700

    add LLAMA Factory install
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+git clone --depth 1 https://github.com/hiyouga/LLaMA-Factory.git
+cd LLaMA-Factory
+pip install -e ".[torch,metrics]"
+
 pip install transformers
 pip install peft
 pip install bitsandbytes
@@ -7,4 +11,3 @@ pip install tf-keras
 pip install --upgrade pandas
 pip install numpy==1.26.4
 pip install gpustat
-
